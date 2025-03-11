940e401a326cc828204b442d75f04b02e8d8cb01
DeepraJ <chandeliyadeepraj@gmail.com>
2025-03-11 23:32:26 +0530

    Hello Hi there

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const prompt = "Tell me how the world is stick to internet?";
+const prompt = "Tell me how to deploy a forntend project on Vercel step by step?";
 
 const generate = async() => {
     try{
